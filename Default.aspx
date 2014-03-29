960ddc805dc8016549b83a01caffac4da9c42d6b
Ankur Tyagi <ankurtyaagi@gmail.com>
2014-03-29 17:36:03 +0530

    Initial-Commit
    First Commit. Actually This is Complete Project!!

---
+++ Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h2>Compiler Online</h2>
+            </hgroup>
+            <p>
+                Your Great Ideas will be Born here</p>
+        </div>
+    </section>
+</asp:Content>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h3>
+        <asp:TextBox ID="tb_code" runat="server" Height="395px" TextMode="MultiLine" ToolTip="This is the place for your code" Width="919px"></asp:TextBox>
+        <asp:Button ID="b_clear" runat="server" OnClick="b_clear_Click" Text="Clear" ToolTip="Click here to clear" Width="116px" />
+        <asp:Button ID="b_submit" runat="server" OnClick="b_submit_Click" Text="Submit" ToolTip="Click here to submit your code" />
+</h3>
+<h3>
+        &nbsp;</h3>
+    </asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void b_submit_Click(object sender, EventArgs e)
+    {
+        // create a writer and open the file
+            TextWriter tw = new StreamWriter("D:\\code.java");
+        // write a line of text to the file
+            tw.WriteLine(tb_code.Text);
+        // close the stream
+            tw.Close();
+            CommandLine cl = new CommandLine();
+           string output = cl.call_cmd("javac code.java");
+        
+           output = cl.call_cmd("java code");
+       // String output = cl.call_cmd(tb_code.Text);
+            tb_code.Text = output;
+    }
+
+    protected void b_clear_Click(object sender, EventArgs e)
+    {
+        tb_code.Text = null;
+    }
+}
+
+public partial class CommandLine : System.Web.UI.Page
+{
+    public String call_cmd(String cmd)
+    {
+        System.Diagnostics.Process si = new System.Diagnostics.Process();
+        si.StartInfo.WorkingDirectory = "d:\\";
+        si.StartInfo.UseShellExecute = false;
+        si.StartInfo.FileName = "cmd.exe";
+        si.StartInfo.Arguments = "/c "+cmd;
+        si.StartInfo.CreateNoWindow = true;
+        si.StartInfo.RedirectStandardInput = true;
+        si.StartInfo.RedirectStandardOutput = true;
+        si.StartInfo.RedirectStandardError = true;
+        si.Start();
+        string output = si.StandardOutput.ReadToEnd();
+        si.Close();
+        return output;
+    }
+}
